646f4db0a0db72f6ee59749ff5603f6368510e4d
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-03-30 23:14:53 +0600

    first commit

---
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
