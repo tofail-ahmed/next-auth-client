3185482f0ff750ce94689065f38a303657559318
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-03-31 11:49:53 +0600

    Implement GitHub Authentication System

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -1,6 +1,12 @@
 import { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
-  providers: [],
+  providers: [
+      GitHubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string
+          })
+  ],
 };
