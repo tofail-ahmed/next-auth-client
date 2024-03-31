15b3206a62c7055c47e69a4f8888455cb6fd949d
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-03-31 19:51:09 +0600

    Implement User Logout Functionality

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -1,5 +1,6 @@
 import { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
@@ -7,7 +8,11 @@ export const authOptions: NextAuthOptions = {
       GitHubProvider({
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string
-          })
+          }),
+          GoogleProvider({
+            clientId: process.env.GOOGLE_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string
+          }),
   ],
   secret:process.env.NEXTAUTH_SECRET
 };
