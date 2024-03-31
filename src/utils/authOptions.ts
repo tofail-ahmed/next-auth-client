13c733c92a9c4e7cfff6962447b3558fcecb965f
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-04-01 00:57:49 +0600

    Protect A Route From Unauthorized Users Using Middleware

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -14,5 +14,8 @@ export const authOptions: NextAuthOptions = {
             clientSecret: process.env.GOOGLE_SECRET as string
           }),
   ],
+  pages:{
+    signIn:"/login",
+  },
   secret:process.env.NEXTAUTH_SECRET
 };
