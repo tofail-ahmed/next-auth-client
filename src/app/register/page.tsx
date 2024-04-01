fac84d5e39785546d4d960235084dd8f9f8d401b
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-04-01 12:12:53 +0600

    Register User Using Next.Js Server Actions And A Custom Server

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { registerUser } from "@/utils/actions/registerUser";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 
 export type UserData = {
@@ -16,11 +18,17 @@ const RegisterPage = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<UserData>();
-
+const router = useRouter();
   const onSubmit = async (data: UserData) => {
-    console.log(data);
+    // console.log(data);
 
     try {
+      const res = await registerUser(data);
+      console.log(res);
+      if (res.success) {
+        alert(res.message);
+        router.push("/login");
+      }
     } catch (err: any) {
       console.error(err.message);
       throw new Error(err.message);
---
+++ src/utils/actions/registerUser.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { UserData } from "@/app/register/page";
+
+export const registerUser = async (data:UserData) => {
+  const res = await fetch(`${process.env.SERVER_URL}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+    cache: "no-store",
+  });
+  const userInfo = await res.json();
+
+  return userInfo;
+};
